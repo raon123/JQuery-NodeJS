caffeb3682c9fb007c60a37ad4523bb39d9864e1
raon123 <37460937+raon123@users.noreply.github.com>
2019-07-14 10:39:22 -0400

    Add files via upload

---
+++ jquery-project-master/js/app.js
@@ -0,0 +1,15 @@
+// var $;
+// var jQuery;
+// console.log($, typeof $);
+// console.log(jQuery, typeof jQuery);
+
+// $("body").append("<h1>Hello World</h1>");
+// $("body").append("<h1>Hello Yusuf</h1>");
+
+// $(document).ready(function(){
+// 	$("body").append("<h1>Hello World</h1>");
+// });
+
+$(function(){
+	$("body").append("<h1>Hello World!!!</h1>");
+});
